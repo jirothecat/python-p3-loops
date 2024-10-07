c3eeb9d2445359e1b7cd188645fb406f0936f754
Dylan Tin <dylant711@gmail.com>
2024-10-07 14:58:36 -0400

    p3 loops

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,33 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    counter = 10
+    while counter > 0:
+        print(counter)
+        counter -= 1
+    print("Happy New Year!")
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    # new_list = []
+    # for element in int_list:
+    #     new_list.append(element ** 2)
+    # or
+    # new_list = [(element ** 2) for element in int_list]
+
+    return [element ** 2 for element in int_list]
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    counter = 1
+    while(counter <= 100):
+        if counter % 3 == 0 and counter % 5 == 0:
+            print("FizzBuzz")
+        elif counter % 3 == 0:
+            print("Fizz")
+        elif counter % 5 == 0:
+            print("Buzz")
+        else:
+            print(counter)
+        counter += 1
+
+fizzbuzz()
+    
